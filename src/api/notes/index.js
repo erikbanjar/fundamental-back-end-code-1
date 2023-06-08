9b1910e73f9d652b694317330ba21c6e7fe2d698
ErikBanjar <erikbanjar76@gmail.com>
2023-06-08 15:02:10 +0700

    fundamental back-end restructure with hapi plugin

---
+++ src/api/notes/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+const NotesHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service }) => {
+    const notesHandler = new NotesHandler(service);
+    server.route(routes(notesHandler));
+  },
+};
